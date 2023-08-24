924ccc37f9105065cee71245487a341af2ce70b0
jaeho <jaeho13@naver.com>
2023-08-24 23:26:54 +0900

    0824 jaeho 프로젝트 스타트

---
+++ web/src/main/java/com/sellent/web/Controller/MainController.java
@@ -0,0 +1,12 @@
+package com.sellent.web.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/hello")
+    public String test() {
+        return "Hello, World!";
+    }
+}
