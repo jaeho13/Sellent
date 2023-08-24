1ce9a63ece0c1aa2893471df26ca2d355bb04522
jaeho <jaeho13@naver.com>
2023-08-24 23:39:03 +0900

    0824 jaeho 메인 화면 구상

--- web/src/main/frontend/src/App.js
+++ web/src/main/frontend/src/App.js
@@ -1,19 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Header/Home";
+
 
 function App() {
-   const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    
+    </>
     );
 }
 
---
+++ web/src/main/frontend/src/components/Header/Home.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+    return (
+        <>
+            <Back>
+                <Bind>
+
+                    <Left>
+                        <LeftTop>로고</LeftTop>
+                        <LeftBoard>메인화면</LeftBoard>
+                        <LeftBoard>재능판매</LeftBoard>
+                        <LeftBoard>재능구매</LeftBoard>
+                    </Left>
+
+                    <Center>
+                        <CenterSearch type="text" placeholder="*재능검색" />
+                    </Center>
+
+                    <Right>
+                        <RightTop>재능구함</RightTop>
+                        <RightBoard />
+                        <RightBoard />
+                        <RightBoard />
+                        <RightBoard />
+                    </Right>
+
+                </Bind>
+            </Back>
+        </>
+    );
+}
+
+export default Home;
+
+const Back = styled.div`
+    width: 85%;
+    height: 85vh;
+    border: 2px solid green;
+    margin: 0 auto;
+    margin-top: 8vh;
+`
+
+const Bind = styled.div`
+    display: flex;
+    justify-content: row;
+`
+
+const Left = styled.div`
+    width: 15%;
+    height: 85vh;
+    border: 2px solid black;
+`
+
+const LeftTop = styled.div`
+    width: 100%;
+    height: 13vh;
+    border: 2px solid black;
+`
+
+const LeftBoard = styled.div`
+    width: 80%;
+    height: 8vh;
+    border: 2px solid blue;
+    margin: 0 auto;
+    margin-top: 2rem;
+    font-size: 2em;
+`
+
+const Center = styled.div`
+    width: 60%;
+    height: 85vh;
+    border: 2px solid red;
+`
+
+const CenterSearch = styled.input`
+    width: 70%;
+    height: 8vh;
+    border: 2px solid black;
+    font-size: 2rem;
+    margin-left: 3em;
+    margin-top: 1em;
+`
+
+const Right = styled.div`
+    width: 25%;
+    height: 85vh;
+    border: 2px solid blue;
+    overflow: auto; /* 스크롤 추가 */
+    overflow-x: hidden; /* 가로 스크롤 제거 */
+`
+
+const RightTop = styled.div`
+    width: 40%;
+    height: 8vh;
+    /* border: 2px solid black; */
+    font-size: 2em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bolder;
+    padding-top: 0.5em;
+    padding-left: 0.5em;
+`
+
+const RightBoard = styled.div`
+    width: 60%;
+    height: 20vh;
+    border: 2px solid red;
+    margin: 0 auto;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+`
