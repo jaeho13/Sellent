70cce4ff195fa832a323eda87cd7f9ed34254db3
kimyuri <dsuis1310009@naver.com>
2023-08-27 23:37:40 +0900

    Kakao Login

---
+++ web/src/main/java/com/sellent/web/Service/NaverService.java
@@ -0,0 +1,8 @@
+package com.sellent.web.Service;
+
+import javax.servlet.http.HttpSession;
+
+public class NaverService {
+//    public String getToken(String code, HttpSession session) {
+//    }
+}
