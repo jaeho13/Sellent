b0c8524664e822986c68c9c1ef01f67883eeb470
jaeho <jaeho13@naver.com>
2023-08-29 00:34:10 +0900

    0829 jaeho 카카오 지도 api 예제 구현

--- web/src/main/frontend/src/components/Header/example.jsx
+++ web/src/main/frontend/src/components/Header/example.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
 const Example = () => {
+
+    useEffect(() => {
+        const kakao = window['kakao'];
+        kakao.maps.load(() => {
+            const mapContainer = document.getElementById('map');
+            const location = new kakao.maps.LatLng(37.568228238811, 126.81350044801);
+            const options = {
+                center: location,
+                level: 3
+            };
+            const map = new kakao.maps.Map(mapContainer, options);
+        });
+    }, []);
+
     return (
         <>
-            <div>아 배고파</div>
+            <Exam id="map" />
         </>
     );
 }
 
 export default Example;
+
+const Exam = styled.div`
+    width: 80%;
+    height: 50rem;
+    margin: 0 auto;
+    margin-top: 5rem;
+`;
