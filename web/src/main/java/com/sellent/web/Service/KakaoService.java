4cf69c7f466852672ced8c0734539eb298c2793d
kimyuri <dsuis1310009@naver.com>
2023-08-29 22:19:24 +0900

    naver login

--- web/src/main/java/com/sellent/web/Service/KakaoService.java
+++ web/src/main/java/com/sellent/web/Service/KakaoService.java
@@ -112,9 +112,6 @@ public class KakaoService {
 
             UserList userCheck = userRepository.findByUserEmail(email);
 
-            if(userCheck.getUserEmail().equals("gohn13@naver.com")){
-                userList.setUserNm("이재포");
-            }
 
             if (userCheck == null) {
                 System.out.println("등록된 회원 아님");
