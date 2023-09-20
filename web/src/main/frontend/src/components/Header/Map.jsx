a89be66cc0db708227bec2af1bc12b8974e4f93e
kimyuri <dsuis1310009@naver.com>
2023-09-20 23:42:27 +0900

    kakaoMap 수정

--- web/src/main/frontend/src/components/Header/Map.jsx
+++ web/src/main/frontend/src/components/Header/Map.jsx
@@ -1,36 +1,31 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 
-const Map = () => {
-
+const Map = ({ locationX, locationY }) => {
     useEffect(() => {
-        const kakao = window['kakao'];
+        const kakao = window.kakao;
         kakao.maps.load(() => {
-            const mapContainer = document.getElementById('map');
-            const location = new kakao.maps.LatLng(37.4953666908089, 127.03306536185);
+            const mapContainer = document.getElementById("map");
+            const location = new kakao.maps.LatLng(locationX, locationY);
             const options = {
                 center: location,
                 level: 3
             };
             const map = new kakao.maps.Map(mapContainer, options);
         });
-    }, []);
+    }, [locationX, locationY]);
 
     return (
-        <>
-            <Exam id="map" />
-        </>
+        <Exam id="map" />
     );
 }
-
-export default Map;
 
 const Exam = styled.div`
     width: 80%;
     height: 20em;
-    /* margin: 0 auto; */
-    /* margin-top: 2em; */
     margin-top: 2em;
     margin-left: 3em;
     margin-bottom: 5em;
 `;
+
+export default Map;
