c88d02ab661a8dfcca3916688266728a493bc35f
jaeho13 <jaehooo13@naver.com>
2023-09-26 23:27:03 +0900

    0926 jaeho 마이페이지 재능구매 수정

--- web/src/main/frontend/src/components/Header/MyPage.jsx
+++ web/src/main/frontend/src/components/Header/MyPage.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import "../fonts/Font.css";
 import { AiFillCloseCircle } from "react-icons/ai"
+import { FcLike } from "react-icons/fc"
 import { useNavigate } from "react-router-dom";
 import Map from "./Map";
+import axios from "axios";
+import NoImage from "../Image/no_img.png";
 
 const MyPage = () => {
 
@@ -35,6 +38,27 @@ const MyPage = () => {
 
     const goSearch = () => {
         navigate("/search")
+    }
+
+    const [purList, setPurList] = useState([]);
+
+    useEffect(() => {
+        const purchaseLoad = async () => {
+            try {
+                const response = await axios.get("/list");
+                setPurList(response.data.purList);
+                console.log("재능 구매 불러오기 성공")
+                console.log(response.data)
+            } catch (error) {
+                console.log("재능 구매 불러오기 실패");
+            }
+        };
+
+        purchaseLoad();
+    }, []);
+
+    const handleSellentRead = (sellIdx) => {
+        navigate(`/sellentRead/${sellIdx}`); //sellIdx에 해당하는 글 읽기 페이지 이동
     }
 
     return (
@@ -70,7 +94,7 @@ const MyPage = () => {
 
                         <DetailsBind>
                             <Review>받은 후기</Review>
-                            <Charege>포인트 충전</Charege>
+                            <Charge>포인트 충전</Charge>
                         </DetailsBind>
 
                         <Where>내 지역</Where>
@@ -79,11 +103,32 @@ const MyPage = () => {
                     </Center>
 
                     <Right>
-                        <RightTop>재능구함</RightTop>
-                        <RightBoard />
-                        <RightBoard />
-                        <RightBoard />
-                        <RightBoard />
+                        <RightTop>재능구매</RightTop>
+
+                        <RightContents>
+                            {purList.length > 0 && purList.map((purItem, index) => {
+                                return (
+                                    <RightBoardBind key={purItem.sellIdx}>
+                                        <RightBoard>
+                                            <BoardImg>
+                                                <img src={NoImage} alt="No Image" />
+                                            </BoardImg>
+                                            <BoardTitle onClick={() => handleSellentRead(purItem.sellIdx)}>
+                                                {purItem.sellTitle.length > 6
+                                                    ? `${purItem.sellTitle.slice(0, 6)}...`
+                                                    : purItem.sellTitle}
+                                                <BoardLike>
+                                                    <FcLike />
+                                                    <LikeScore>
+                                                        {purItem.sellLike}
+                                                    </LikeScore>
+                                                </BoardLike>
+                                            </BoardTitle>
+                                        </RightBoard>
+                                    </RightBoardBind>
+                                )
+                            })}
+                        </RightContents>
                     </Right>
                 </Bind>
             </Back>
@@ -223,7 +268,7 @@ const Name = styled.div`
     }
 `
 const Center = styled.div`
-    width: 60%;
+    width: 55%;
     height: 85vh;
     border: 2px solid red;
     background-color: white;
@@ -290,7 +335,7 @@ const Review = styled.div`
 `
 
 
-const Charege = styled.div`
+const Charge = styled.div`
     width: 28%;
     height: 5vh;
     border: 2px solid black;
@@ -312,10 +357,50 @@ const Where = styled.div`
     align-items: center;
 `
 
+const BoardImg = styled.div`
+    width: 100%;
+    height: 15vh;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    /* border: 2px solid red; */
+`
+
+const BoardTitle = styled.div`
+    width: 100%;
+    height: 4vh;
+    /* border: 2px solid red; */
+    border-top: 2px solid black;
+    font-size: 1.5em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+`
+
+const BoardLike = styled.div`
+    width: 30%;
+    height: 4vh;
+    /* border: 2px solid green; */
+    font-size: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const LikeScore = styled.div`
+    width: 30%;
+    height: 4vh;
+    /* border: 2px solid black; */
+    display: flex;
+    align-items: center;
+`
 
 
 const Right = styled.div`
-    width: 25%;
+    width: 30%;
     height: 85vh;
     border: 2px solid blue;
     overflow: auto; /* 스크롤 추가 */
@@ -324,23 +409,40 @@ const Right = styled.div`
 `
 
 const RightTop = styled.div`
-    width: 40%;
-    height: 8vh;
+    width: 100%;
+    height: 5vh;
     /* border: 2px solid black; */
-    font-size: 2rem;
+    border-bottom: 2px solid black;
+    font-size: 2.5rem;
     display: flex;
-    justify-content: center;
+    /* justify-content: center; */
     align-items: center;
     font-weight: bolder;
-    padding-top: 0.5rem;
+    padding-top: 0.5em;
     padding-left: 0.5rem;
+    color: #595959;
+`
+
+const RightContents = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    /* 넘치는 경우 줄바꿈 */
+`
+
+const RightBoardBind = styled.div`
+    width: 50%;
+    display: flex;
+    margin-bottom: 2rem;
 `
 
 const RightBoard = styled.div`
-    width: 60%;
+    width: 100%;
     height: 20vh;
-    border: 2px solid red;
-    margin: 0 auto;
-    margin-top: 1.5rem;
-    margin-bottom: 1.5rem;
+    border: 2px solid black;
+    margin-top: 2rem;
+    /* margin-left: 5rem; */
+    margin-left: 1rem;
+    margin-right: 1rem;
 `
